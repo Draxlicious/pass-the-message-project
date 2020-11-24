9bbf510c77fbedb24723c8511142ec1c031a9536
Lukas Mark Jakobsen <43439073+Draxlicious@users.noreply.github.com>
2020-11-24 13:21:38 +0100

    second attempt for practise

--- js/app.js
+++ js/app.js
@@ -18,5 +18,25 @@ form.addEventListener("submit", function(e){
 
 })
 
+// let form = document.querySelector("#message-form")
+// let nameInput = document.querySelector("#message")
+// let feedback = document.querySelector(".feedback")
+// let message = document.querySelector(".message-content")
+
+// form.addEventListener("submit", function(e){
+// e.preventDefault();
+
+// if(nameInput.value === ""){
+//     feedback.classList.add("show")
+//     setTimeout(() => {
+//         feedback.classList.remove("show")
+//     }, 2000);
+// }else{
+//     message.innerHTML = nameInput.value
+//     nameInput.value = "";
+// }
+
+// })
+
 
     
